a5450986d89d10705310b994cb0eb3ad1edf34bf
qasim-techcolleagues <mqsaleem1999@gmail.com>
2023-09-18 20:13:21 +0500

    add packages and student class

---
+++ SchoolSystem1.Infrastructure/Model/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolSystem1.Infrastructure.Model
+{
+    public class Student
+    {
+        public int Id { get; set; } 
+        public string StudentName { get; set; }
+        public string FatherName { get; set; }
+    }
+}
