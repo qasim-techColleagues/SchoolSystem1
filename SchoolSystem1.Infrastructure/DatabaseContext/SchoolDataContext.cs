23e691bb0ca98a50b574a1bcd9ccd82d3d97e176
qasim-techcolleagues <mqsaleem1999@gmail.com>
2023-09-18 20:27:55 +0500

    Add dbcontext

---
+++ SchoolSystem1.Infrastructure/DatabaseContext/SchoolDataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolSystem1.Infrastructure.Model;
+
+namespace SchoolSystem1.Infrastructure.DatabaseContext
+{
+    public class SchoolDataContext : DbContext
+    {
+       public DbSet<Student> Students { get; set; }
+    }
+}
