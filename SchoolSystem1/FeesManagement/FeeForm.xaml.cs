3de2cd8ad09144e49052af31423217f530783728
zubair-techcollegues <zubair.liaqat2701@gmail.com>
2023-09-21 17:06:03 +0500

    Add FeeManagement

---
+++ SchoolSystem1/FeesManagement/FeeForm.xaml.cs
@@ -0,0 +1,76 @@
+﻿using SchoolSystem1.StudentManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SchoolSystem1.FeesManagement
+{
+    /// <summary>
+    /// Interaction logic for FeeForm.xaml
+    /// </summary>
+    public partial class FeeForm : Window
+    {
+        private Fee _fee;
+        private Student _student;
+        public FeeForm()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Fee = new Fee();
+            studentNamelist.ItemsSource = StudentList.Students;
+        }
+
+        public Fee Fee
+        {
+            get => _fee;
+            set => _fee = value;
+        }
+        public Student SelectedStudent
+        {
+            get => _student;
+            set
+            {
+                _student = value;
+
+                if (value != null)
+                {
+                    var fee =FeeList.Fees.Where(x => x.StudentId == value.StudentId).FirstOrDefault();
+                    if (fee != null)
+                        Fee = fee;
+
+                }
+            }
+        }
+
+        private void btn_feesubmit_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectedStudent == null)
+                return;
+            var fee = FeeList.Fees.Where(x => x.StudentId == SelectedStudent.StudentId).FirstOrDefault();
+            if (fee == null)
+            {
+                Fee.Studentname = SelectedStudent.StudentName;
+                Fee.StudentId = SelectedStudent.StudentId;
+                Fee.StudentFather = SelectedStudent.FatherName;
+                FeeList.Fees.Add(Fee);
+            }
+            else
+            {
+                fee = Fee;
+            }
+            this.Close();
+
+
+        }
+    }
+}
---
+++ SchoolSystem1/FeesManagement/FeeList.xaml.cs
@@ -0,0 +1,56 @@
+﻿using SchoolSystem1.StudentManagement;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SchoolSystem1.FeesManagement
+{
+    /// <summary>
+    /// Interaction logic for FeeList.xaml
+    /// </summary>
+    public partial class FeeList : Page
+    {
+        static FeeList()
+        {
+            Fees = new ObservableCollection<Fee>();
+        }
+        public FeeList()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+        public static ObservableCollection<Fee> Fees { get; set; }
+
+        private void addfee_Click(object sender, RoutedEventArgs e)
+        {
+            var feestudent = new FeeForm();
+            feestudent.ShowDialog();
+
+        }
+
+        private void dgfeelist_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (dgfeelist != null)
+            {
+                Fee selectedfee = dgfeelist.SelectedItem as Fee;
+                FeeForm feeSystem = new FeeForm();
+                feeSystem.Fee = selectedfee;
+                feeSystem.SelectedStudent = StudentList.Students.Where(x => x.StudentId == selectedfee.StudentId).First();
+                feeSystem.ShowDialog();
+            }
+
+        }
+    }
+}
